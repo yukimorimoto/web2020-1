584ab33316bfe4322b1c3bbd9b9576a2aae95f79
yukimorimoto <zm18075@josai.ac.jp>
2020-02-11 09:04:35 +0000

    2/11

--- aisatsu.js
+++ aisatsu.js
@@ -2,3 +2,11 @@ function sayhello(){
     alert('Hello,' +
     document.getElementById('who').value + '!');
 }
+
+function ThreeSecChange(){
+    setInterval("imgchange12()",3000);
+}
+
+function imgchange12(){
+    document.getElementById('logo2').src ='./image/house.png';
+}
